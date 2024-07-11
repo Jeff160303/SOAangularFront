08697071e758e9b6440e6805c81fdb5b740fd20d
Jefferson <jeffersonolaechea160303@gmail.com>
2024-07-11 01:16:34 -0500

    actualiza las cantidades luego de una venta y elimina los productos del carrito.

--- src/app/pages/ventas/exito/exito.component.ts
+++ src/app/pages/ventas/exito/exito.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
@@ -11,7 +11,11 @@ import Swal from 'sweetalert2';
   templateUrl: './exito.component.html',
   styleUrl: './exito.component.css'
 })
-export class ExitoComponent{
+export class ExitoComponent implements OnInit {
+
+  cargando: boolean = false;
+  userData: any;
+  carritos: any[] = [];
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -30,22 +34,22 @@ export class ExitoComponent{
       showConfirmButton: false
     });
 
-    const userData = JSON.parse(localStorage.getItem('userData') || '{}');
-    const carritos = JSON.parse(localStorage.getItem('carritos') || '[]');
+    this.userData = JSON.parse(localStorage.getItem('userData') || '{}');
+    this.carritos = JSON.parse(localStorage.getItem('carritos') || '[]');
 
-    if (userData && carritos.length > 0) {
+    if (this.userData && this.carritos.length > 0) {
       const venta = {
         fechaEmision: new Date(),
-        dni: userData.dni,
-        nombres: userData.nombres,
-        apellidos: userData.apellidos,
+        dni: this.userData.dni,
+        nombres: this.userData.nombres,
+        apellidos: this.userData.apellidos,
         tipoVenta: 'pedido',
         direccion: 'Dirección del usuario',
-        total: this.calcularTotalAmount(carritos),
+        total: this.calcularTotalAmount(this.carritos),
         estado: 'pendiente'
       };
 
-      const detalles = carritos.map((carrito: any) => ({
+      const detalles = this.carritos.map((carrito: any) => ({
         nombreProducto: carrito.nombreProducto,
         cantProducto: carrito.cantidadProducto,
         tallaProducto: carrito.talla,
@@ -53,7 +57,7 @@ export class ExitoComponent{
       }));
 
       const payload = {
-        email: userData.correo,
+        email: this.userData.correo,
         venta: venta,
         detalles: detalles
       };
@@ -69,7 +73,7 @@ export class ExitoComponent{
               text: 'La boleta ha sido enviada por correo.',
               icon: 'success'
             }).then(() => {
-              // Redireccionar o realizar otras acciones
+              this.actualizarCantidades(this.carritos);
             });
           },
           error => {
@@ -90,11 +94,74 @@ export class ExitoComponent{
     }
   }
 
+  actualizarCantidades(carritos: any[]): void {
+    const actualizaciones = carritos.map((carrito: any) => ({
+      idProducto: carrito.idProducto,
+      talla: carrito.talla,
+      cantidad: carrito.cantidadProducto
+    }));
+
+    this.http.put('http://localhost:8080/productos/actualizarCantidades', actualizaciones)
+      .subscribe(
+        response => {
+          console.log('Cantidades actualizadas exitosamente:', response);
+          this.vaciarCarrito(carritos);
+        },
+        error => {
+          console.error('Error al actualizar cantidades:', error);
+          Swal.fire({
+            title: 'Error',
+            text: 'Ocurrió un error al actualizar las cantidades de los productos.',
+            icon: 'error'
+          });
+        }
+      );
+  }
+
+  vaciarCarrito(carritos: any[]): void {
+    const eliminarProductoPromises = carritos.map((carrito: any) => this.eliminarProducto(carrito.idCarrito));
+    Promise.all(eliminarProductoPromises)
+      .then(() => {
+        console.log('Carrito vaciado exitosamente.');
+        this.router.navigate(['/']);
+      })
+      .catch(error => {
+        console.error('Error al vaciar el carrito:', error);
+        Swal.fire({
+          title: 'Error',
+          text: 'Ocurrió un error al vaciar el carrito.',
+          icon: 'error'
+        });
+      });
+  }
+
+  eliminarProducto(idCarrito: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.cargando = true;
+      this.http.delete(`http://localhost:8070/carrito/eliminar/${idCarrito}`).subscribe(
+        response => {
+          console.log('Producto eliminado del carrito:', response);
+          setTimeout(() => {
+            this.cargando = false;
+            resolve();
+          }, 300);
+        },
+        error => {
+          console.error('Error al eliminar producto del carrito:', error);
+          setTimeout(() => {
+            this.cargando = false;
+            reject(error);
+          }, 300);
+        }
+      );
+    });
+  }
+
   calcularTotalAmount(carritos: any[]): number {
     return carritos.reduce((total, carrito) => total + (carrito.cantidadProducto * carrito.precioProducto), 0);
   }
 
-  inicio(){
+  inicio(): void {
     this.router.navigateByUrl('/');
   }
 
